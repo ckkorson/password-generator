5caf53437f36e931b808a24ab76b66a876d74d9c
Caleb Korson <caleb@Calebs-MacBook-Air.local>
2022-08-16 09:00:47 -0400

    Got password generation to work and intergrated with button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,29 +10,72 @@ var lowerCaseList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
 var numberList = [0,1,2,3,4,5,6,7,8,9]
 var symbolList = ["!","#","$","%","&","*","+",'-',"/","?","@","=","_"]
 var passwordLength = 0
+var newCharList = []
+var listLength = 0
+var passwordString = []
+
 function lengthInput() {
   while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("Please enter how many characters you would like you password." + "\n" +
     "Enter a number that is at least 8 and no more than 128")
   }
 }
-lengthInput()
 
+function inputUpperCase() {
+  return confirm("Would you like to use upper case letters?")
+}
 
+function inputLowerCase() {
+  return confirm("Would you like to use lower case letters?")
+}
+
+function inputnumber() {
+  return confirm("Would you like to use numbers?")
+}
+
+function inputsymbol() {
+  return confirm("Would you like to use symbols?")
+}
 
 function getRandom() {
-  return Math.floor(Math.random() * 75)
+  return Math.floor(Math.random() * listLength)
 }
-var password = []
-function writePassword2() {
-  for (var i = 0; i < passwordLength; i++) {
-    password.push(fullCharacterList[getRandom()])
+
+function createCharList() {
+  if (inputUpperCase()) {
+    newCharList = newCharList.concat(upperCaseList)
+  }
+  if (inputLowerCase()) {
+    newCharList = newCharList.concat(lowerCaseList)
+  }
+  if (inputnumber()) {
+    newCharList = newCharList.concat(numberList)
+  }
+  if (inputsymbol()) {
+    newCharList = newCharList.concat(symbolList)
   }
 }
-writePassword2()
-// password.shift()
-console.log(password.join(""))
 
+function findListLength() {
+  listLength = newCharList.length
+}
+
+function createPassword() {
+  for (var i = 0; i < passwordLength; i++) {
+    passwordString.push(newCharList[getRandom()])
+  }
+}
+// password.shift()
+function generatePassword() {
+  lengthInput()
+  createCharList()
+  findListLength()
+  createPassword()
+  return passwordString.join("")
+}
+// console.log(passwordString.join(""))
+// generatePassword()
+// console.log(passwordSting)
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
